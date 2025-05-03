f2d93c0951416de4ee568f014c29740ec86ed2ee
ejjays <christsonalloso021@gmail.com>
2025-05-03 20:12:53 +0800

    ...

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ export const dynamic = 'force-dynamic';
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import InterviewCard from "@/components/cards/InterviewCard";
+import InterviewCard from "@/components/InterviewCard";
 import { getCurrentUser } from "@/lib/actions/auth.action";
 import {
   getInterviewsByUserId,
